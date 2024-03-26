c16e9c039e3069be1265f0d74740c3c02f92f530
Vladimir Vrzić <vvrzic@gmail.com>
2024-03-26 19:28:22 +0100

    Added guard against division by zero

--- src/Node.php
+++ src/Node.php
@@ -2,40 +2,42 @@
 
 namespace RendezvousHashing;
 
-    class Node
+class Node
+{
+    private $name;
+    private $weight;
+
+    public function __construct(string $name, float $weight)
     {
-        private $name;
-        private $weight;
-
-        public function __construct(string $name, float $weight)
-        {
-            $this->name = $name;
-            $this->weight = $weight;
-        }
-
-        public function getName(): string
-        {
-            return $this->name;
-        }
-
-        public function getWeight(): float
-        {
-            return $this->weight;
-        }
-
-        public function weightedScore(string $key): float
-        {
-            $score = self::hashToUnitInterval($this->name . ': ' . $key);
-            return $this->weight / (-log($score));
-        }
-
-        private static function hashToUnitInterval(string $s): float
-        {
-            return (crc32($s) + 1) / (1 << 32);
-        }
-
-        public function __toString(): string
-        {
-            return "name={$this->name}, weight={$this->weight}";
-        }
+        $this->name = $name;
+        $this->weight = $weight;
     }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getWeight(): float
+    {
+        return $this->weight;
+    }
+
+    public function weightedScore(string $key): float
+    {
+        $hash = self::hashToUnitInterval($this->name . ': ' . $key);
+        $logHash = log($hash);
+        $score = $logHash ? -1 / $logHash : 0;
+        return $this->weight * $score;
+    }
+
+    private static function hashToUnitInterval(string $s): float
+    {
+        return (crc32($s) + 1) / (1 << 32);
+    }
+
+    public function __toString(): string
+    {
+        return "name={$this->name}, weight={$this->weight}";
+    }
+}
